f7f059fa54f3dc738e409be34ce674f55c4c29af
Ulrich Bönkemeyer <boenkemeyer@plansysteme.de>
2018-12-07 17:55:04 +0100

    Removed accidentally added /dist dir

--- dist/README.md
+++
@@ -1,4 +0,0 @@
-# ModelStage Angular Package
-
-© 2018 Plansysteme Gesellschaft für Softwareentwicklung mbH, Hamburg, Germany
-All Rights Reserved.
--- dist/dist2/public_api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/ps-geometry';
-export * from './src/mx-common';
-export * from './src/mx-appstate';
-export * from './src/modelstage';
-export * from './src/modelstage-sample-web';
